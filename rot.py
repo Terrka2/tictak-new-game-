b595b171ca7724363dfb6b2cb6eece152868b1a3
Nichita Speriuc <nekit.sperci7@gmail.com>
2026-09-07 17:46:25 +0300

    added multiplayer

--- rot.py
+++ rot.py
@@ -23,6 +23,25 @@ def bot(table:list):
         table[a] = "o"
     return table
 
+def player(table:list, name1:str, name2:str):
+    players = (name1,name2)
+    show(table)
+    global n
+    answer = int (input("Ходит игрок "+ players[n]+ " : "))
+    while True:
+        if answer in table and isinstance(answer, int):
+            if n == 0:
+                n += 1
+                table[answer - 1] = "x"
+                return table
+            elif n == 1:
+                table[answer - 1] = "o"
+                n = 0
+                return table
+        else: 
+            print("Вы не можете так ходить! ") 
+            break
+
 def result(table:list):
     wins = [
         (0,1,2), (3,4,5), (6,7,8),
@@ -37,38 +56,63 @@ def result(table:list):
     return None
 
 
+
 table = [a for a in range(1,10)]
 
-show(table)
-while True:
-    
-    answer = int(input("Выберите куда хотите пойти или 0 для завершения: "))
-    if answer in table and isinstance(table[answer-1], int):
-        table[answer-1] = "x"
-        winner = result(table)
 
-        if winner == "x":
-            show(table)
-            print("\n Вы победили!")
-            break
-        elif winner == "draw":
-            show(table)
-            print("ничья")
-            break
+a = int(input("Хотите играть с другом или с ботом? \n1 - друг, 2 - бот\n"))
+if a == 2:
 
-        bot(table)
-        show(table)
-        winner = result(table)
-        if winner == "o":
-            print("\n Вы проебали боту!")
+    while True:
+        answer = int(input("Выберите куда хотите пойти или 0 для завершения: "))
+        if answer in table and isinstance(table[answer-1], int):
+            table[answer-1] = "x"
+            winner = result(table)
+
+            if winner == "x":
+                show(table)
+                print("\n Вы победили!")
+                break
+            elif winner == "draw":
+                show(table)
+                print("ничья")
+                break
+            bot(table)
+            show(table)
+            winner = result(table)
+            if winner == "o":
+                print("\n Вы проебали боту!")
+                break
+            elif winner == "draw":
+                print("ничья")
+                break
+            
+        elif answer == 0:
+            print("игра окончена!")
             break
-        elif winner == "draw":
-            print("ничья")
-            break
-        
-    elif answer == 0:
-        print("игра окончена!")
-        break
+        else:
+            print ("Вы не можете так ходить!")
+elif a == 1:
+    name1 = input("Игрок 1 введи свое имя: ")
+    name2 = input("Игрок 2 введи свое имя: ")
+    if name1 == name2:
+        print("Имена не могут быть похожими")
     else:
-        print ("Вы не можете так ходить!")
-    
+        n = 0
+        while True:
+            player(table, name1, name2)
+            show(table)
+            winner = result(table)
+            if winner == "x":
+                show(table)
+                print("\n Поздравляю " , name1 ," победил!")
+                break
+            elif winner == "o":
+                show(table)
+                print("\n Поздравляю " , name2 ," победил!")
+                break
+            elif winner == "draw":
+                show(table)
+                print("ничья")
+                break
+            
